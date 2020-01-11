0fcc8dc6763256c7f38ad9be20172d53488847b8
Richard Lyon <richardlyon@fastmail.com>
2020-01-11 18:51:04 +0000

    replace ArduinoJason with structure, and arduino-LoRa with RadioHead

--- src/main.cpp
+++ src/main.cpp
@@ -4,140 +4,127 @@
  *
  *  Firmware for a LoRa based sensor. Detects which sensors are connected - access is 
  *  via "CompositeSensor" object which provides a unified interface for different sensor 
- *  types. Packages readings in a JSON string, serialises it, and transmits it over LoRa.
+ *  types. CompositeSensor returns a data structure. Transmit this over LoRa.
  * 
- *  Set configuration variables in 'config.h'. 
- * 
- *  NOTE: Implements 'sleep' function between data transmission, which disables 
- *  the USB serial line. Reset the device before flashing.
+ *  Two flashes: data received
+ *  Four flashes: data not received
  * 
  */
-#include <LoRa.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <RHReliableDatagram.h>
+#include <RH_RF95.h>
 #include <Ticker.h>
-#include <Adafruit_SleepyDog.h>
-#include <ArduinoJson.h>
 
 #include "sensor-utils.h"
 #include "CompositeSensor.h"
 
-#define DEVICE_ID "outside" // comment out if device has Sys.DeviceID()
+#define DEVICE_ID "Greenhouse" // comment out if device has Sys.DeviceID()
 
-#define FIRMWARE_VERSION "0.22"
+// Radiohead addresses
+#define SERVER_ADDRESS 1
+// #define CLIENT_ADDRESS 2 // Outside
+#define CLIENT_ADDRESS 3 // Greenhouse
+
+#define FIRMWARE_VERSION "0.23"
 #define SENSOR_TYPE "External Environment Sensor"
 #define FIRMWARE_SLUG "sensor_environment_external-32U4RFM95LORA-arduino"
 #define FIRMWARE_MCU "32U4RFM95LORA"
 #define FIRMWARE_OS "arduino"
 
-// set true to sleep between transmissions to conserve battery
-#define SLEEP_MODE false
 // number of seconds between transmissions
 #define SLEEP_SECONDS 3
 
 #define DEBUG true // set false to suppress debug info on Serial
 
+// Radiohead pinsfor feather32u4
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+
 // board pin assignments
 #define LED_BUILTIN 13 // red onboard LED
-
-// feather32u4 LoRa pin assignment
-#define NSS 8    // New Slave Select pin
-#define NRESET 4 // Reset pin
-#define DIO0 7   // DIO0 pin
-
-const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5);
-StaticJsonDocument<capacity> doc;
-JsonObject device = doc.createNestedObject("device");
-JsonObject data = doc.createNestedObject("data");
 
 // Initialise sensors
 CompositeSensor mySensor;
 
-unsigned int packetID;
+// Embedis
+const size_t EEPROM_SIZE = E2END + 1;
+// Embedis embedis(Serial);
+
+// Singleton instance of the radio driver
+RH_RF95 driver(RFM95_CS, RFM95_INT);
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, CLIENT_ADDRESS);
+
+// Timers for LED flashing
+void flashLED(int times, int millis);
 
 void setup()
 {
+  digitalWrite(LED_BUILTIN, LOW);
+
   Serial.begin(115200);
   delay(2000);
 
-  LoRa.setPins(NSS, NRESET, DIO0);
-  digitalWrite(LED_BUILTIN, LOW);
-
   utils::printBanner(SENSOR_TYPE, FIRMWARE_SLUG, FIRMWARE_VERSION, DEVICE_ID);
 
-  mySensor.begin();
-  if (mySensor.hasSCD30)
-    Serial.println("Sensor: SCD30 (Temperature / Humidity / CO2)");
-  if (mySensor.hasHDC1080)
-    Serial.println("Sensor: HDC1080 (Temperature / Humidity)");
-  if (mySensor.hasBMP388)
-    Serial.println("Sensor: BMP388 (Pressure / Temperature)");
-  if (mySensor.hasVEML7700)
-    Serial.println("Sensor: VEML7700 (Lux)");
-
-  if (!LoRa.begin(433E6))
+  // start LoRa
+  if (manager.init())
   {
-    Serial.println("Starting LoRa failed.");
-    while (1)
-      ;
+    Serial.print("LoRa client started, ID:");
+    Serial.println(CLIENT_ADDRESS);
   }
-  Serial.println("LoRa started");
-  packetID = 0;
+
+  mySensor.begin();
+  // readSensors.start();
 }
 
 void loop()
 {
   // code resumes here on wake.
 
+  CompositeSensor::SensorReadings readings = mySensor.readSensors();
+
   // serialise the JSON for transmission
-  char serialData[255];
+  uint8_t serialData[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
-  ++packetID;
-  doc["packetID"] = packetID;
-  doc["status"] = 1;
+  byte zize = sizeof(readings);
+  memcpy(buf, &readings, zize);
 
-  // device
-  device["id"] = DEVICE_ID;
-  device["version"] = FIRMWARE_VERSION;
-  device["volts"] = mySensor.readBattery();
-
-  // data
-  data["temp"] = mySensor.readTemperature();
-  data["humidity"] = mySensor.readHumidity();
-  data["co2"] = mySensor.readCO2();
-  data["lux"] = mySensor.readLight();
-  data["mbars"] = mySensor.readPressure();
-
-  serializeJson(doc, serialData);
-
-  // send in async / non-blocking mode
-  while (LoRa.beginPacket() == 0)
+  // send message to manager server
+  if (manager.sendtoWait(buf, zize, SERVER_ADDRESS))
   {
-    delay(100);
-  }
-  LoRa.beginPacket();
-  LoRa.print(serialData);
-  LoRa.endPacket(true); // true = async / non-blocking mode
-
-  // signal transmission
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(50);
-  digitalWrite(LED_BUILTIN, LOW);
-
-  // log to serial port
-  Serial.println("TX Packet: ");
-  if (DEBUG)
-  {
-    Serial.println("---");
-    serializeJsonPretty(doc, Serial);
-    Serial.println();
-  }
-
-  // sleep
-  if (SLEEP_MODE)
-  {
-    Watchdog.sleep(SLEEP_SECONDS * 1000);
+    // Now wait for a reply from the server
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
+    {
+      Serial.print("Server:");
+      Serial.println((char *)buf);
+      flashLED(CLIENT_ADDRESS, 150);
+    }
+    else
+    {
+      Serial.println("No reply, is rf95_reliable_datagram_server running?");
+      flashLED(1, 1000);
+    }
   }
   else
+    Serial.println("sendtoWait failed");
+
+  delay(SLEEP_SECONDS * 1000);
+}
+
+void flashLED(int times, int millis)
+{
+  for (int i = 0; i < times; ++i)
   {
-    delay(SLEEP_SECONDS * 1000);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(millis);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(millis);
   }
 }
